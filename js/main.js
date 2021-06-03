4e72af408a85de21afd169ce332c66d6af822678
RicentCB <ricardo.cb009@gmail.com>
2021-06-03 14:58:40 -0500

    [ADD] Funcion para validar datos

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // Objeto para el envio de datos
   let addEventParams = {
     "title": '',
-    "room": '',
+    "room": '0',
     "color": '',
     "subject": '',
     "date": '',
@@ -33,8 +33,8 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   // Construir Calendario
-  const calendarEl = document.getElementById('calendar');
-  const calendar = new FullCalendar.Calendar(calendarEl, {
+  const calendarDOM = document.getElementById('calendar');
+  const calendar = new FullCalendar.Calendar(calendarDOM, {
     initialView: 'dayGridMonth',
     locale: 'es',
     headerToolbar: {
@@ -65,6 +65,21 @@ document.addEventListener('DOMContentLoaded', function () {
     modalAddEvent.querySelector(".modal-content").classList.remove('visible');
   })
 
+  //Escribir titulo
+  modalAddEvent.querySelector("#in-title").addEventListener('change', e=>{
+    addEventParams["title"] = e.target.value;
+  });
+
+  //Seleccionar Salon
+  modalAddEvent.querySelector("#in-room").addEventListener('change', e=>{
+    addEventParams["room"] = e.target.value;
+  });
+  
+  //Seleccionar Materia
+  modalAddEvent.querySelector("#in-subject").addEventListener('change', e=>{
+    addEventParams["subject"] = e.target.value;
+  });
+
   // Seleccionar Color
   modalAddEvent.querySelectorAll("a.option-color").forEach(domElement=>{
     domElement.addEventListener("click", e=>{
@@ -84,9 +99,29 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   })
 
+  //Seleccionar Dia
+  modalAddEvent.querySelector("#in-date").addEventListener('change', e=>{
+    addEventParams["date"] = e.target.value;
+  });
+
+  //Seleccionar Hora Inicial
+  modalAddEvent.querySelector("#in-timeStart").addEventListener('change', e=>{
+    addEventParams["timeStart"] = e.target.value;
+  });
+
+  //Seleccionar Hora Final
+  modalAddEvent.querySelector("#in-timeEnd").addEventListener('change', e=>{
+    addEventParams["timeEnd"] = e.target.value;
+  });
+
+  const infoModal = modalAddEvent.querySelector('.alert');
   // Boton de Enviar Datos en modal presionado
   modalAddEvent.querySelector("a.btn.submit").addEventListener("click", e=>{
     e.preventDefault();
+    //Validaciones
+    if(formIsValid()){
+      console.log("valido")
+    }
     let data = new FormData();
     data.append("var1", "val1");
     data.append("var2", "val2");
@@ -95,6 +130,70 @@ document.addEventListener('DOMContentLoaded', function () {
     //   console.log(ans);
     // })
   });
+
+    //Funcion para validar datos de formularip
+  const formIsValid = ()=>{
+    infoModal.classList.remove('active');
+
+    //Creacion de fechas para compara horas de inicio y fin
+    let dateStart = new Date(addEventParams["date"]+' '+addEventParams["timeStart"]+':00');
+    let dateEnd = new Date(addEventParams["date"]+' '+addEventParams["timeEnd"]+':00');
+    
+    if(addEventParams["title"].trim() === ''){
+      infoModal.innerHTML = 'El titulo es obligatario';
+      infoModal.classList.add('active');
+      modalAddEvent.querySelector("#in-title").focus();
+      return false;
+    }
+    if(addEventParams["room"] == "0"){
+      infoModal.innerHTML = 'Seleccione un salon';
+      infoModal.classList.add('active');
+      modalAddEvent.querySelector("#in-room").focus();
+      return false;
+    }
+    if(modalAddEvent.querySelector("#in-subject").value.trim() === ''){
+      infoModal.innerHTML = 'La unidad de aprendizaje es obligatoria';
+      infoModal.classList.add('active');
+      modalAddEvent.querySelector("#in-subject").focus();
+      return false;
+    }
+    if(addEventParams["color"] === ''){
+      infoModal.innerHTML = 'Seleccione un color';
+      infoModal.classList.add('active');
+      return false;
+    }
+    if(addEventParams["color"] === ''){
+      infoModal.innerHTML = 'Seleccione un color';
+      infoModal.classList.add('active');
+      return false;
+    }
+    if(addEventParams["date"] === ''){
+      infoModal.innerHTML = 'Eliga una fecha';
+      infoModal.classList.add('active');
+      modalAddEvent.querySelector("#in-date").focus();
+      return false;
+    }
+    if(addEventParams["timeStart"] === ''){
+      infoModal.innerHTML = 'Eliga hora de inicio';
+      infoModal.classList.add('active');
+      modalAddEvent.querySelector("#in-timeStart").focus();
+      return false;
+    }
+    if(addEventParams["timeEnd"] === ''){
+      infoModal.innerHTML = 'Eliga hora de termino';
+      infoModal.classList.add('active');
+      modalAddEvent.querySelector("#in-timeEnd").focus();
+      return false;
+    }
+    if(dateStart >= dateEnd){
+      infoModal.innerHTML = 'Revisa las horas de inicio y fin';
+      infoModal.classList.add('active');
+      return false;
+    }
+    return true;
+
+  }
+
 });
 
 // Metodo Post para enviar datos usando url y formData
