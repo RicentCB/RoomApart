52c05bd672a9123dbcea4e44f68cfe6797cb2e95
RicentCB <ricardo.cb009@gmail.com>
2021-06-03 09:34:28 -0500

    [ADD] Cerrar modal

--- js/main.js
+++ js/main.js
@@ -1,15 +1,84 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-
-    const calendarEl = document.getElementById('calendar');
-    const calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      locale: 'es',
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-      },
-    });
-    calendar.render();
+  // Construir Calendario
+  const calendarEl = document.getElementById('calendar');
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    locale: 'es',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+    },
+    events: eventsEx,
   });
+  calendar.render();
+
+  // Modal para agregar evento
+  const modalAddEvent = document.querySelector(".modal#modal-addEvent")
+  
+  modalAddEvent.querySelector("a.close-modal").addEventListener('click', e=>{
+    e.preventDefault();
+    console.log("Cerrar");
+  })
+
+});
+
+const eventsEx = [
+  {
+    title: 'All Day Event',
+    start: '2021-06-01',
+    backgroundColor: "#ccc",
+    borderColor: "#ff0095",
+  },
+  {
+    title: 'Long Event',
+    start: '2021-06-07',
+    end: '2021-06-10'
+  },
+  {
+    groupId: 999,
+    title: 'Repeating Event',
+    start: '2021-06-09T16:00:00'
+  },
+  {
+    groupId: 999,
+    title: 'Repeating Event',
+    start: '2021-06-16T16:00:00'
+  },
+  {
+    title: 'Conference',
+    start: '2021-06-11',
+    end: '2021-06-13'
+  },
+  {
+    title: 'Meeting',
+    start: '2021-06-12T10:30:00',
+    end: '2021-06-12T12:30:00'
+  },
+  {
+    title: 'Lunch',
+    start: '2021-06-12T12:00:00'
+  },
+  {
+    title: 'Meeting',
+    start: '2021-06-12T14:30:00'
+  },
+  {
+    title: 'Happy Hour',
+    start: '2021-06-12T17:30:00'
+  },
+  {
+    title: 'Dinner',
+    start: '2021-06-12T20:00:00'
+  },
+  {
+    title: 'Birthday Party',
+    start: '2021-06-13T07:00:00'
+  },
+  {
+    title: 'Click for Google',
+    url: 'http://google.com/',
+    start: '2021-06-28'
+  }
+]
