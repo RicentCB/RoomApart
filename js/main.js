ca3e4e05d1ebe0c08d08c048ff280b1cbf71e96f
RicentCB <ricardo.cb009@gmail.com>
2021-06-02 22:22:21 -0500

    [FIRST COMMIT]

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    const calendarEl = document.getElementById('calendar');
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      locale: 'es',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+      },
+    });
+    calendar.render();
+  });
